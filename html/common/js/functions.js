d1ddca532676fef2c761bca7e7c005dd3c7d3ff4
h2ham <hasegawa@ham.works>
2016-09-18 15:20:59 +0900

    add globalnav style

--- html/common/js/functions.js
+++ html/common/js/functions.js
@@ -29,48 +29,8 @@ HW = function() {
 
       base.breakpointClassSwitch();
 
-      var runSwitch = false;
-      if ($('#sampleStyle').length > 0) {
-        runSwitch = true;
-        $('#run').text('Break');
-        $('#cssCode').text($.trim($('#sampleStyle').html()));
-      }
-      $('#run').on('click', function () {
-        if (runSwitch) {
-          $('style').remove();
-          $(this).text('Run');
-          runSwitch = false;
-        } else {
-          var css = $('#cssCode').html().replace(/&gt;/g,'>');
-          var html = $('#htmlCode').clone().html();
-          $('<style></style>').html(css).appendTo('head');
-          $(this).text('Break');
-          if (html){
-            $('.demo').html(html.replace(/&gt;/g,'>').replace(/&lt;/g,'<'));
-          }
-          runSwitch = true;
-        }
-      });
-
-      $('#cssCode, #htmlCode').on('click', function () {
-        if (!$(this).hasClass('on')){
-          var codeHeight = $(this).height()+6;
-          $(this).addClass('on');
-          var txt = $(this).text();
-          $(this).html('<textarea>'+txt+'</textarea>');
-          $('textarea').css({height:codeHeight}).focus().blur(function(){
-            var inputVal = $(this).val();
-            $(this).parent().removeClass('on').text(inputVal);
-          });
-        }
-      });
-
-      if ($('#htmlCode').length > 0) {
-        $('#htmlCode').text($.trim($('.demo').html().replace(/&gt;/g,'>').replace(/&lt;/g,'<')));
-      }
-
-      $(document).on('click', 'button, .js-button, .menu i', function() {
-        $('body').toggleClass('move');
+      $(document).on('click', '.js-nav-button', function() {
+        $('body').toggleClass('nav-is-show');
       });
 
       // スクロール時のイベント
